4f84973d61ed2c94c6b18511f67117329bc5ef40
Kenny Mustapha <Kennymustapha2005@gmail.com>
2026-07-31 13:54:33 -0700

    sends back

--- web-server.py
+++ web-server.py
@@ -8,4 +8,8 @@ print("Server ready to receive")
 while True:
     (clientSocket, addr) = serverSocket.accept()
     request = clientSocket.recv(1024).decode()
+    filename = request.split()[1]
+    body = open(filename[1:], 'rb').read()
+    clientSocket.send(b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n")
+    clientSocket.send(body)
     clientSocket.close()
